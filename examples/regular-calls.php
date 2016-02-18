12e58de6c221ac2bdc50a0720a7b87e077022dec
Štefan Janckulík <pysta@neonus.sk>
2016-02-18 08:58:21 +0100

    added examples

---
+++ examples/regular-calls.php
@@ -0,0 +1,40 @@
+<?php
+use Neonus\Neoship\NeoshipSdk;
+
+$clientId = '2_osoowoogoo0gwoko00kggkws';
+$clientSecret = '34cooc0480c4scw0gkko48wcsk444g';
+$redirectUri = 'http://www.example.com/neoship/redirect.php';
+
+$neoship = new NeoshipSdk($clientId, $clientSecret, $redirectUri);
+
+// edits existing package with ID 40272 via regular API call
+$neoship->apiPutPackage('40272', array(
+    'package' => array(
+        'sender' => array(
+            'appelation' => 'Mr',
+            'name' => 'Jeffe',
+            'company' => 'Jeffe S.r.o.',
+            'street' => 'Slnecna',
+            'city' => 'Namestovo',
+            'houseNumber' => '158',
+            'houseNumberExt' => null,
+            'zIP' => '02901',
+            'state' => 1,
+            'email' => null,
+            'phone' => null,
+        ),
+        'reciever' => array(
+            'appelation' => 'Mr',
+            'name' => 'Jeffe',
+            'company' => 'Jeffe S.r.o.',
+            'street' => 'Slnecna',
+            'city' => 'Namestovo',
+            'houseNumber' => '158',
+            'houseNumberExt' => null,
+            'zIP' => '02901',
+            'state' => 1,
+            'email' => null,
+            'phone' => null,
+        ),
+    ),
+));
